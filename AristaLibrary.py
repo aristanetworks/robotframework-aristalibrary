a6fbaf524492b4c93b4f834f090fa0db3074bfb6
sugetha24 <sugethakch@arista.com>
2015-02-04 17:54:59 -0600

    code cleanups: pep8 and pyflakes

--- AristaLibrary.py
+++ AristaLibrary.py
@@ -1,37 +1,43 @@
 import jsonrpclib
 
+
 def create_rpc(url):
     eapi = jsonrpclib.Server(url)
     return eapi
 
-def connect_to(node, proto="https", port="443", username="admin", passwd="admin"):
+
+def connect_to(node, proto="https", port="443", username="admin",
+               passwd="admin"):
     """Try to connect to an EOS node over JSONRPC"""
 
-    url = "%s://%s:%s@%s:%s/command-api" % (proto, username, passwd, node, port)
+    url = "%s://%s:%s@%s:%s/command-api" % \
+          (proto, username, passwd, node, port)
     eapi = jsonrpclib.Server(url)
 
     try:
-      result = eapi.runCmds(1, ["show version"])
-      return url
+        result = eapi.runCmds(1, ["show version"])
+        return url
     except jsonrpclib.ProtocolError as e:
-      errorResponse = jsonrpclib.loads(jsonrpclib.history.response)
-      print "Failed:", errorResponse["error"]["data"][0]["errors"][-1]
+        errorResponse = jsonrpclib.loads(jsonrpclib.history.response)
+        print "Failed:", errorResponse["error"]["data"][0]["errors"][-1]
 
-def eos_version_is(version,url):
+
+def eos_version_is(version, url):
     """Checks if EOS software version equals specified value"""
 
     eapi = create_rpc(url)
 
     try:
-      result = eapi.runCmds(1, ["show version"])
+        result = eapi.runCmds(1, ["show version"])
     except jsonrpclib.ProtocolError as e:
-      errorResponse = jsonrpclib.loads(jsonrpclib.history.response)
-      print "Failed:", errorResponse["error"]["data"][0]["errors"][-1]
+        errorResponse = jsonrpclib.loads(jsonrpclib.history.response)
+        print "Failed:", errorResponse["error"]["data"][0]["errors"][-1]
 
     result = result[0]
     nodeVersion = result["version"]
     if nodeVersion != version:
-      raise AssertionError('Incorrect Software version found')
+        raise AssertionError('Incorrect Software version found')
+
 
 def extension_installed(name, version, available, installed, url):
     """Checks if an extension has the right values"""
@@ -39,10 +45,10 @@ def extension_installed(name, version, available, installed, url):
     eapi = create_rpc(url)
 
     try:
-      result = eapi.runCmds(1, ["show extensions"], "text")
+        result = eapi.runCmds(1, ["show extensions"], "text")
     except jsonrpclib.ProtocolError as e:
-      errorResponse = jsonrpclib.loads(jsonrpclib.history.response)
-      print "Failed:", errorResponse["error"]["data"][0]["errors"][-1]
+        errorResponse = jsonrpclib.loads(jsonrpclib.history.response)
+        print "Failed:", errorResponse["error"]["data"][0]["errors"][-1]
 
     result = result[0]['output']
     lines = [i for i in result.split('\n')]
@@ -58,11 +64,11 @@ def extension_installed(name, version, available, installed, url):
         rpmParts = [i for i in line.split()]
         if len(rpmParts) > 3:
             d = {
-              'name':rpmParts[0],
-              'version':rpmParts[1],
-              'available':rpmParts[2].replace(",",""),
-              'installed':rpmParts[3],
-              'rpms':rpmParts[4]
+                'name': rpmParts[0],
+                'version': rpmParts[1],
+                'available': rpmParts[2].replace(",", ""),
+                'installed': rpmParts[3],
+                'rpms': rpmParts[4]
             }
             rpms.append(d)
 
@@ -76,46 +82,53 @@ def extension_installed(name, version, available, installed, url):
             r = rpm
             if rpm['version'] != version:
                 success = False
-                log += "\nVersion is NOT a match: %s != %s" % (rpm['version'], version)
+                log += "\nVersion is NOT a match: %s != %s" % \
+                       (rpm['version'], version)
 
             if rpm['available'] != available:
                 success = False
-                log += "\nAvailable is NOT a match: %s != %s" % (rpm['available'], available)
+                log += "\nAvailable is NOT a match: %s != %s" % \
+                       (rpm['available'], available)
 
             if rpm['installed'] != installed:
                 success = False
-                log += "\nInstalled is NOT a match: %s != %s" % (rpm['installed'], installed)
+                log += "\nInstalled is NOT a match: %s != %s" % \
+                       (rpm['installed'], installed)
     if success:
         return True
-    elif extFound:        raise AssertionError("Extension found but with params:%s\n" % log)
+    elif extFound:
+        raise AssertionError("Extension found but with params:%s\n" % log)
     else:
-        raise AssertionError("Extension not found in returned in eAPI command\n"
-                             "RawOutput:\n%s" % result)
+        raise AssertionError(
+            "Extension not found in returned in eAPI command\n"
+            "RawOutput:\n%s" % result)
 
-def mlag_state_is(state,url):
+
+def mlag_state_is(state, url):
     """Checks if MLAG is in the proper state"""
 
     eapi = create_rpc(url)
 
     try:
-      result = eapi.runCmds(1, ["show mlag"])
+        result = eapi.runCmds(1, ["show mlag"])
     except:
-      raise AssertionError('Cannot connect to EOS node')
+        raise AssertionError('Cannot connect to EOS node')
 
     result = result[0]
     mlagState = result['state']
     if mlagState != state:
-      raise AssertionError('Incorrect Software version found')
+        raise AssertionError('Incorrect Software version found')
 
-def node_env_cooling_between(min,max,url):
+
+def node_env_cooling_between(min, max, url):
     """Checks if Node has cooling values between min and max"""
 
     eapi = create_rpc(url)
     try:
-      result = eapi.runCmds(1, ["show environment cooling"])
+        result = eapi.runCmds(1, ["show environment cooling"])
     except jsonrpclib.ProtocolError as e:
-      errorResponse = jsonrpclib.loads(jsonrpclib.history.response)
-      print "Failed:", errorResponse["error"]["data"][0]["errors"][-1]
+        errorResponse = jsonrpclib.loads(jsonrpclib.history.response)
+        print "Failed:", errorResponse["error"]["data"][0]["errors"][-1]
 
     min = int(min)
     max = int(max)
@@ -124,7 +137,7 @@ def node_env_cooling_between(min,max,url):
     for traySlot in result["fanTraySlots"]:
         for fan in traySlot["fans"]:
             if (fan["configuredSpeed"] not in range(min, max) or
-                fan["actualSpeed"] not in range(min, max)):
+               fan["actualSpeed"] not in range(min, max)):
                 raise AssertionError('Value found not in range:%s to %s'
                                      'reported value:%s %s' %
                                      (min, max, fan["configuredSpeed"],

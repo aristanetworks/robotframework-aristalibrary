b9538019be6c9a6f4ccda3cd7049c4bc78ecef10
sugetha24 <sugethakch@arista.com>
2015-02-06 12:30:08 -0600

    Class structure; connect_to and enable keywords.

--- AristaLibrary.py
+++ AristaLibrary.py
@@ -11,14 +11,14 @@ class AristaLibrary:
         self.passwd = passwd
 
     def connect_to(self, proto, hostname, username, passwd, port):
+        proto = str(proto)
+        hostname = str(hostname)
+        username = str(username)
+        passwd = str(passwd)
+        port = str(port)
         node = pyeapi.connect(proto, hostname, username, passwd, port)
-        out = node.execute(['show version'])
-        return out
+        return node
 
     def enable(self, conn, command):
         return conn.execute([command])
 
-arista = AristaLibrary()
-node = arista.connect_to('https', '10.68.49.140', 'admin', 'admin', '443')
-#out = arista.enable(node, 'show version')
-print node
